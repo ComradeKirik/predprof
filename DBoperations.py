28ebf75d1a4b773b36ee10bec7305392dc4fd19d
TowarichKirik <lysovkirillanatolievich@gmail.com>
2025-12-20 22:06:33 +0300

    Я заебался, логин с ошибкой когда пытаешься залогиниться, что-то там с сессией, это надо будет изучить

--- DBoperations.py
+++ DBoperations.py
@@ -1,10 +1,9 @@
 import psycopg2
 from psycopg2.extras import DictCursor
+
 conn = psycopg2.connect(host="localhost", user="postgres", password="TK", port=5432, dbname="players")
 if conn:
     print("Connected")
-
-
 
 cursor = conn.cursor()
 
@@ -16,7 +15,12 @@ def checkUserName(username):
     cursor.execute("SELECT * FROM registered_players WHERE player_name = %s", (username,))
     return cursor.fetchone()
 
-def addNewUser(username, email, password):
-    cursor.execute("INSERT INTO registered_players(player_score, player_name, player_password, email) VALUES (1000, %s, %s, %s,)",\
-                   (username, password, email))
+def addNewUser(username, email, password_hash):
+    cursor.execute("INSERT INTO registered_players(player_score, player_name, player_password, email) VALUES (1000, %s, %s, %s)",\
+                   (username, password_hash, email))
     conn.commit()
+
+def loginUser(username, password_hash):
+    cursor.execute("SELECT * FROM registered_players WHERE player_name = %s AND player_password = %s", \
+                   (username, password_hash))
+    return cursor.fetchone()
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, blueprints, request, render_template
+from flask import Flask, blueprints, request, render_template, session
 from hashlib import sha256
 import re
 import DBoperations
@@ -9,23 +9,43 @@ app = Flask(__name__)
 def mainpage():
     return render_template('main.html')
 
-@app.route("/register", methods="POST")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    msg = ""
+    if request.method == "POST" and "username" in request.form and "password" in request.form:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_hash = sha256(password.encode('utf-8')).hexdigest()
+        account = DBoperations.loginUser(username, password_hash)
+        if account:
+            session['loggedin'] = True
+            session['id'] = account[0]
+            session['username'] = account[1]
+            msg = "Успешный вход!"
+        if account == None:
+            msg = "Аккаунта не существует или введен некорректный пароль!"
+    return render_template('login.html', msg=msg)
+
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     msg = ""
     if request.method == "POST" and "username" in request.form and "password" in request.form and "email" in request.form:
         username = request.form.get("username")
-        password = sha256(request.form.get("password"))
+        password = request.form.get("password")
         email = request.form.get("email")
+        password_hash = sha256(password.encode('utf-8')).hexdigest()
+
         if DBoperations.checkUserEmail(email) != None:
             msg = "Данная почта уже зарегистрирована"
-        elif not re.match(r"[A-Za-z0-9_]", username):
+        elif not re.match(r"^[A-Za-z0-9_]+$", username):
             msg = "Запрещенные символы в имени. Разрешена латиница, цифры и _"
         elif DBoperations.checkUserName(username) != None:
             msg = "Данный никнейм уже используется!"
         elif not username or not email or not password:
             msg = "Пожалуйста, заполните все поля!"
         else:
-            DBoperations.addNewUser(username, email, password)
+            DBoperations.addNewUser(username, email, password_hash)
             msg = "Успешная регистрация!"
 
     return render_template('register.html', msg=msg)
